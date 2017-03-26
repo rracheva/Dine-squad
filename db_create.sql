1199ee6ff19c7381c87c550639e7b8145e9b0174
tonyramos <rar32013@mymail.pomona.edu>
2017-03-26 16:31:46 -0700

    added cuisine and dish tables

--- db_create.sql
+++ db_create.sql
@@ -8,20 +8,30 @@ USE DINE;
 CREATE TABLE EmailPreferences(
 	emailid INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(256) NOT NULL,
-	preferneces VARCHAR(256) --a string, possibly seperate prefs by commas/spaces
+	preferences VARCHAR(256) --a string, possibly seperate prefs by commas/spaces
 );
 
 CREATE TABLE DiningRatings (
 	drid INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	dininghall VARCHAR(256) NOT NULL,
 	rating INT UNSIGNED NOT NULL
-)
+);
 
 -- ask rali for more info 
-CREATE TABLE Preferences(
-	pid INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Cuisine(
+	cid INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	type VARCHAR(256) NOT NULL,
 
-)
+);
+
+-- might switch to excel file
+CREATE TABLE Dishes(
+	dishid INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	dname VARCHAR(256) NOT NULL,
+	cid INT UNSIGNED NOT NULL ,
+	FOREIGN KEY (cid) REFERENCES Cuisine(cid)
+
+);
 
 -- check scheme
 Describe EmailPreferences;
